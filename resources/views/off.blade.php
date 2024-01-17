65708d647d14d432c2339a0f285cd6d4c90fd1a0
Much Yusron Arif <3467343+yusronarif@users.noreply.github.com>
2024-01-17 08:39:48 +0700

    fix message handler

--- resources/views/off.blade.php
+++ resources/views/off.blade.php
@@ -2,7 +2,7 @@
   $style = $attributes->get('style', 'width:100%;min-height:400px;display:flex;justify-content: center;align-items: center;font-size: calc(1rem + 1vmin);color: #ccc;font-style: italic;');
   $content = $slot;
   if ($content->isEmpty()) {
-    $content = !empty($message)
+    $content = ! empty($message)
     ? $message
     : 'Metabase is disabled on local\'s environment';
   }
